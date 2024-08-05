da05775013ddd70bd9b9e9b948656b9ad251ed65
Muhammad Hassaan <115350907+hasn20@users.noreply.github.com>
2024-08-05 09:32:50 +0500

    a

--- public/script.js
+++ public/script.js
@@ -1,27 +1,9 @@
-/*jshint esversion: 8 */
-
-const p = document.querySelector("#visit-counter");
-const VISITORS_ENDPOINT = "https://2dctl4rwl7.execute-api.us-east-1.amazonaws.com/prod"; // Replace with your actual API Gateway endpoint
-
-const updateCounter = async (event) => {
-  try {
-    const response = await fetch(VISITORS_ENDPOINT, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const visitors = await response.json();
-    p.textContent = `This page has been visited ${visitors.visits} times`;
-  } catch (error) {
-    p.textContent = "Unable to retrieve the count.";
-    console.error("Error fetching visit count:", error);
-  }
-};
-
-window.addEventListener("DOMContentLoaded", updateCounter);
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch(
+        "https://wwjcx7tyxrbjmbkf3vc3teo3mu0qrvhq.lambda-url.ca-central-1.on.aws/"
+    );
+    let data = await response.json();
+    counter.innerHTML = `👀 Views: ${data}`;
+}
+updateCounter();
