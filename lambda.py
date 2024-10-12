f2d71931a23a997f45f816f61032207b2737bbfd
Muhammad Hassaan <115350907+hasn20@users.noreply.github.com>
2024-10-12 12:50:16 +0500

    update

---
+++ lambda.py
@@ -0,0 +1,23 @@
+4. **Lambda Function**
+   The following Python code is used in the AWS Lambda function to update the visitor count:
+
+   ```python
+   import json
+   import boto3
+
+   dynamodb = boto3.resource('dynamodb')
+   table = dynamodb.Table('resume-challenge')
+
+   def lambda_handler(event, context):
+       response = table.get_item(Key={
+           'id':'0'
+       })
+       views = response['Item']['views']
+       views = views + 1
+       print(views)
+       response = table.put_item(Item={
+               'id':'0',
+               'views': views
+       })
+       return views
+   ```
